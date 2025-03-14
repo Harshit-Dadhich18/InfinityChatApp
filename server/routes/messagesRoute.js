1b6e56d94a7a7484aeb09321a20dee845cc93ede
HARSHIT <hdadheech46@gmail.com>
2025-03-14 15:55:32 +0530

    Delete server directory

--- server/routes/messagesRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { addMessage, getAllMessage } = require('../controllers/messageController');
-const router = express.Router();
-
-router.post("/addmsg/",addMessage);
-router.post("/getmsg/",getAllMessage);
-
-
-module.exports = router;
